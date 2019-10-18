d3b4f74fa288bbed57ac1e163e6b479c36c8f749
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-10-18 05:57:56 -0300

    imprime primeiro autor

--- src/br/com/alura/kotlin/collection/list/ManipulaListas.kt
+++ src/br/com/alura/kotlin/collection/list/ManipulaListas.kt
@@ -4,18 +4,24 @@ fun main() {
     val autores = listOf("João", "Helena", "Joaquim")
 
     // Percorre todos os autores
+    println("# Lista Completa #")
     for(autor in autores) {
         println(autor)
     }
 
+    println("\n# Primeiro Autor #")
+
     // Acessa primeiro autor via Index Operator
     val primeiroAutorIndexOperator = autores[0]
+    println(primeiroAutorIndexOperator)
 
     // Acessa primeiro autor via método get
     val primeiroAutorGet = autores.get(0)
+    println(primeiroAutorGet)
 
     // Acessa primeiro autor via função first
     val primeiroAutorFirstFun = autores.first()
+    println(primeiroAutorFirstFun)
 
 
     val outrosAutores: List<String> = listOf<String>("Carlos", "José", "Euclides")

49034f5a89f1a090335e53f15c9490b0e057d51c
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-10-22 10:06:10 -0300

    percorrendo elementos do array

--- src/br/com/alura/kotlin/collection/array/ManipulaArrays.kt
+++ src/br/com/alura/kotlin/collection/array/ManipulaArrays.kt
@@ -16,26 +16,53 @@ fun main() {
     val multiplosDeMil = LongArray(10) { it * 1000L }
     println(multiplosDeMil.joinToString())
 
+
     // Outras funções para construir e inicializar arrays de valores primitivos
-    println(byteArrayOf(-128, 0, 1, 2, 127).joinToString())
-    println(shortArrayOf(-32768, 0, 1, 2, 32767).joinToString())
-    println(charArrayOf('\u0000', 'a', 'b', 'c', '\uffff').joinToString())
-    println(intArrayOf(-2_147_483_648, 0, 42, 2_147_483_647).joinToString())
-    println(longArrayOf(-9_223_372_036_854_775_807, 10L, 123_456_789_10, 9_223_372_036_854_775_807).joinToString())
-    println(doubleArrayOf(987.1, .2, 123412342143.123143214).joinToString())
-    println(booleanArrayOf(true, true, false).joinToString())
+
+    val bytes = byteArrayOf(-128, 0, 1, 2, 127)
+    val idades = shortArrayOf(10, 19, 40, 67)
+    val alfabeto = charArrayOf('a', 'b', 'c', 'd', 'e')
+    val fibonacci = intArrayOf(1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89)
+    val arrayDeMilissegundos = longArrayOf(10000000093333, 23400000093333, 33904392035517)
+    val valores = doubleArrayOf(10.55, 29.99, 10000.00)
+    val respostas = booleanArrayOf(true, true, false, true, false)
+
 
     // Acessando elementos do array
 
-    val primeiroElemento = arrayDeIndices[0] // O índice inicia em zero
-    println(primeiroElemento)
+    val primeiraLetraDoAlfabeto = alfabeto[0]
 
-    val ultimoElemento = arrayDeIndices[9] // O último índice é igual ao tamanho do array -1
-    println(ultimoElemento)
+    val quartoElementoSequenciaDeFibonacci = fibonacci[3]
 
-    arrayDeIndices[arrayDeIndices.size -1] // Outra forma de acessar o último elemento do array
+    val ultimaResposta = respostas[respostas.size - 1]
+
 
     // Percorrendo arrays
+
+    for(i in 0..fibonacci.size) { // ArrayIndexOutOfBoundsException
+        print("${fibonacci[i]} - ")
+    }
+
+    for(i in 0..(fibonacci.size -1)) {
+        print("${fibonacci[i]} - ")
+    }
+
+    for(i in 0 until valores.size) {
+        val valorFormatado = "%.2f".format(valores[i]) // formata para duas casas decimais
+        println("R$ $valorFormatado")
+    }
+
+    var maiorIdade: Short = 0
+
+    for(idade in idades) {
+        if(maiorIdade < idade) {
+            maiorIdade = idade
+        }
+    }
+
+    println("Maior idade: $maiorIdade")
+
+    println("Maior idade: ${idades.max()}")
 
     // Alterando arrays
 

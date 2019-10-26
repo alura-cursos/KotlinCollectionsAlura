ab85c61c869073e4cd704aae1d216bc9853e6455
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-10-26 10:54:24 -0300

    filtra idades

--- src/br/com/alura/kotlin/collection/array/Idades.kt
+++ src/br/com/alura/kotlin/collection/array/Idades.kt
@@ -1,7 +1,7 @@
 package br.com.alura.kotlin.collection.array
 
 fun main() {
-    val idades = intArrayOf(10, 19, 33, 40, 67)
+    val idades = intArrayOf(10, 12, 18, 33, 40, 67)
     println("Idades: ${idades.joinToString(prefix = "[", postfix = "]")}")
 
     println("Maior idade: ${idades.max()}")
@@ -9,12 +9,21 @@ fun main() {
     val mediaIdades = idades.average()
     println("Média das idades: $mediaIdades")
 
-    val todosMaiores = idades.all { it > 18 }
+    val todosMaiores = idades.all { it >= 18 }
     println("Todos são maiores de idade? $todosMaiores")
 
     val existeMenor = idades.any { it < 18 }
     println("Existe algum menor de idade? $existeMenor")
 
+    val maiores = idades.filter { it >= 18 }
+    println("Maiores de idade: $maiores")
+
+    val menores = idades.filter { it < 18 }
+    println("Menores de idade: $menores")
+
     val contemQuarenta = idades.contains(40)
     println("Existe alguem com 40 anos? $contemQuarenta")
+
+    val busca = idades.find { it == 33 }
+    println("Idade encontrada: $busca")
 }

de93b82c912fc85538954e3bd93b58964cd62a10
dmagdaleno <diegommagdaleno@gmail.com>
2019-11-05 13:26:52 -0300

    separa função para criar lista de livros

--- src/br/com/alura/kotlin/collection/list/Prateleira.kt
+++
@@ -1,6 +0,0 @@
-package br.com.alura.kotlin.collection.list
-
-data class Prateleira (
-    val genero: String,
-    val livros: MutableList<Livro>
-)
---
+++ src/br/com/alura/kotlin/collection/list/livros.kt
@@ -0,0 +1,12 @@
+package br.com.alura.kotlin.collection.list
+
+fun getLivros() = mutableListOf(
+    Livro(nome = "Grande Sertão: Veredas", autor = "João Guimarães Rosa", anoPublicacao = 1956),
+    Livro(nome = "Minha vida de menina", autor = "Helena Morley", anoPublicacao = 1942, editora = "Editora A"),
+    Livro(nome = "Memórias Póstumas de Brás Cubas", autor = "Machado de Assis", anoPublicacao = 1881),
+    Livro(nome = "Sagarana", autor = "João Guimarães Rosa", anoPublicacao = 1946),
+    Livro(nome = "Iracema", autor = "José de Alencar", anoPublicacao = 1865, editora = "Editora B"),
+    Livro(nome = "Vidas Secas", autor = "Graciliano Ramos", anoPublicacao = 1938, editora = "Editora A"),
+    Livro(nome = "Mayombe", autor = "Pepetela", anoPublicacao = 1979, editora = "Editora B"),
+    Livro(nome = "O Cortiço", autor = "Aluísio Azevedo", anoPublicacao = 1890, editora = "Editora B")
+)

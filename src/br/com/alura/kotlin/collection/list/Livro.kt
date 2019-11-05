63f681505a34553b2425a16472ed3f0c978ca99b
dmagdaleno <diegommagdaleno@gmail.com>
2019-11-05 11:23:02 -0300

    melhora formatação de código

--- src/br/com/alura/kotlin/collection/list/Livro.kt
+++ src/br/com/alura/kotlin/collection/list/Livro.kt
@@ -1,3 +1,8 @@
 package br.com.alura.kotlin.collection.list
 
-data class Livro(val nome: String, val autor: String, val anoPublicacao: Long, val editora: String? = null)
+data class Livro(
+    val nome: String,
+    val autor: String,
+    val anoPublicacao: Long,
+    val editora: String? = null
+)

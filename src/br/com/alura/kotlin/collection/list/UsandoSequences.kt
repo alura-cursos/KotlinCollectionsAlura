92d7a8f91f7cae0ebd23b76c5e33274e079e42e1
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-11-05 22:33:54 -0300

    lista grande de livros com sequence

---
+++ src/br/com/alura/kotlin/collection/list/UsandoSequences.kt
@@ -0,0 +1,30 @@
+package br.com.alura.kotlin.collection.list
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    val listaGrandeLivros = List(2000) {
+        Livro(nome = "Livro $it", autor = "Autor", anoPublicacao = it.toLong(), editora = "Editora $it")
+    }
+
+    val tempoList = measureTimeMillis {
+        listaGrandeLivros
+                .filter { it.anoPublicacao > 1000 }
+                .sortedBy { it.autor }
+                .map { "Título: ${it.nome}" }
+                .forEach(::print)
+    }
+
+    val tempoSequence = measureTimeMillis {
+        listaGrandeLivros
+                .asSequence()
+                .filter { it.anoPublicacao > 1000 }
+                .sortedBy { it.autor }
+                .map { "Título: ${it.nome}" }
+                .forEach(::print)
+    }
+
+    println()
+    println("Tempo List: $tempoList")
+    println("Tempo Sequence: $tempoSequence")
+}

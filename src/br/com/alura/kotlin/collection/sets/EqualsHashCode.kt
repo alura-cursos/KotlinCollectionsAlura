da26b2419cec7852eae1218f385c4315aa46499b
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-11-15 12:15:44 -0300

    explica equals e hashcode

--- src/br/com/alura/kotlin/collection/sets/EqualsHashCode.kt
+++ src/br/com/alura/kotlin/collection/sets/EqualsHashCode.kt
@@ -1,4 +1,49 @@
 package br.com.alura.kotlin.collection.sets
 
-class EqualsHashCode {
+
+class SemEquals(val atributo: Int)
+
+class ComEquals(val atributo: Int) {
+    override fun equals(other: Any?) = other is ComEquals && other.atributo == atributo
+}
+
+class ComEqualsEHashCode(val atributo: Int) {
+    override fun equals(other: Any?) = other is ComEqualsEHashCode && other.atributo == atributo
+    override fun hashCode() = atributo
+}
+
+data class DataClass(val atributo: Int)
+
+fun main() {
+    val conjuntoSemEquals = mutableSetOf(
+            SemEquals(10),
+            SemEquals(10),
+            SemEquals(42),
+            SemEquals(42)
+    )
+    println(conjuntoSemEquals.size)
+
+    val conjuntoComEquals = mutableSetOf(
+            ComEquals(10),
+            ComEquals(10),
+            ComEquals(42),
+            ComEquals(42)
+    )
+    println(conjuntoComEquals.size)
+
+    val conjuntoComEqualsEHashCode = mutableSetOf(
+            ComEqualsEHashCode(10),
+            ComEqualsEHashCode(10),
+            ComEqualsEHashCode(42),
+            ComEqualsEHashCode(42)
+    )
+    println(conjuntoComEqualsEHashCode.size)
+
+    val conjuntoDataClass = mutableSetOf(
+            DataClass(10),
+            DataClass(10),
+            DataClass(42),
+            DataClass(42)
+    )
+    println(conjuntoDataClass.size)
 }

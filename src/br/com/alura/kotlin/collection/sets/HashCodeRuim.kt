06d2194cd5e12361cec79831e1fe194d22d387a9
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-11-15 19:25:44 -0300

    exemplo de hashcode ruim

---
+++ src/br/com/alura/kotlin/collection/sets/HashCodeRuim.kt
@@ -0,0 +1,37 @@
+package br.com.alura.kotlin.collection.sets
+
+class HashCodeBom(val propriedade: Int) {
+    companion object {
+        var contador = 0
+    }
+
+    override fun equals(other: Any?): Boolean {
+        contador++
+        return other is ComEqualsEHashCode && other.propriedade == propriedade
+    }
+
+    override fun hashCode() = propriedade
+}
+
+class HashCodeRuim(val propriedade: Int) {
+    companion object {
+        var contador = 0
+    }
+
+    override fun equals(other: Any?): Boolean {
+        contador++
+        return other is ComEqualsEHashCode && other.propriedade == propriedade
+    }
+
+    override fun hashCode() = 42
+}
+
+fun main() {
+    val conjunto = List(10000) { HashCodeBom(it) }.toSet()
+    HashCodeBom(1000) in conjunto
+    println("Chamadas a equals: ${HashCodeBom.contador}")
+
+    val conjuntoIneficiente = List(10000) { HashCodeRuim(it) }.toSet()
+    HashCodeRuim(1000) in conjuntoIneficiente
+    println("Chamadas a equals: ${HashCodeRuim.contador}")
+}

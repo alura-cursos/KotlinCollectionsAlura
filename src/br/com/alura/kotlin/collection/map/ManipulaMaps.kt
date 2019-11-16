65ea2619e9a959e5477791ae4ab209652e8eb614
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-11-16 20:17:41 -0300

    manipulação básica de maps

---
+++ src/br/com/alura/kotlin/collection/map/ManipulaMaps.kt
@@ -0,0 +1,36 @@
+package br.com.alura.kotlin.collection.map
+
+fun main() {
+    val coisas = mutableMapOf(
+            "fruta" to "abacaxi",
+            "animal" to "cachorro",
+            "pais" to "Brasil"
+    )
+
+    println(coisas)
+    println(coisas["fruta"])
+
+    coisas["fruta"] = "banana"
+    println(coisas)
+
+    coisas["estado"] = "São Paulo"
+    println(coisas)
+
+    coisas["cidade"] = "São Paulo"
+    println(coisas)
+
+    val animal by coisas
+    println(animal)
+
+    println(animal in coisas)
+    println("animal" in coisas)
+
+    val letraEBinario = listOf('a', 'l', 'u', 'r', 'a')
+            .map { it to Integer.toBinaryString(it.toInt()) }
+            .toMap()
+
+    println(letraEBinario)
+
+
+
+}
--- src/br/com/alura/kotlin/collection/map/Map.kt
+++
@@ -1,4 +0,0 @@
-package br.com.alura.kotlin.collection.map
-
-class Map {
-}

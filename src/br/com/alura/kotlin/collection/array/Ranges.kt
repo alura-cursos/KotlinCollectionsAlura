5e1164999c22e97121199cdf1868387d07d91271
dmagdaleno <diegommagdaleno@gmail.com>
2019-11-19 15:12:49 -0300

    adiciona tipo explícito nos ranges

--- src/br/com/alura/kotlin/collection/array/Ranges.kt
+++ src/br/com/alura/kotlin/collection/array/Ranges.kt
@@ -1,29 +1,29 @@
 package br.com.alura.kotlin.collection.array
 
 fun main() {
-    val serie = 1.rangeTo(10)
+    val serie: IntRange = 1.rangeTo(10)
     println(serie)
     for (i in serie) {
         print("$i ")
     }
 
-    val umADez = 1..10
+    val umADez: IntRange = 1..10
     println("\n$umADez")
     umADez.forEach { print("$it ") }
 
-    val numerosPares = 2..100 step 2
+    val numerosPares: IntProgression = 2..100 step 2
     println("\n$numerosPares")
     numerosPares.forEach { print("$it ") }
 
-    val multiplosDeTres = 99 downTo 3 step 3
+    val multiplosDeTres: IntProgression = 99 downTo 3 step 3
     println("\n$multiplosDeTres")
     multiplosDeTres.forEach { print("$it ") }
 
-    val deZeroANove = 0 until 10
+    val deZeroANove: IntRange = 0 until 10
     println("\n$deZeroANove")
     deZeroANove.forEach { print("$it ") }
 
-    val alfabeto = 'a'..'z'
+    val alfabeto: CharRange = 'a'..'z'
     println("\n$alfabeto")
     alfabeto.forEach { print("$it ") }
 }
